1597f4356c961c6cb690cfd50813acdd6f0433e3
Eduardo <el_alienx@hotmail.com>
2023-09-19 15:51:28 +0200

    added json data

--- pages/cars/[id].jsx
+++ pages/cars/[id].jsx
@@ -1,7 +1,33 @@
 // Node modules
 import { useRouter } from "next/router";
 
-export default function CarPage() {
+// SSR
+export async function getServerSideProps({ params }) {
+  const request = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await request.json();
+
+  return { props: { car: data } };
+}
+
+// SSG
+// export async function getStaticProps({ params }) {
+//   const request = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await request.json();
+
+//   return { props: { car: data } };
+// }
+
+// export async function getStaticPaths() {
+//   const request = await fetch("http://localhost:3000/cars.json");
+//   const data = await request.json();
+//   const paths = data.map((item) => {
+//     return { params: { id: item } };
+//   });
+
+//   return { paths, fallback: false };
+// }
+
+export default function CarPage({ car }) {
   // Global state
   const router = useRouter();
   const { id } = router.query;
@@ -9,6 +35,7 @@ export default function CarPage() {
   return (
     <div>
       <h1>Car id #{id}</h1>
+      <img src={car.image} />
     </div>
   );
 }
